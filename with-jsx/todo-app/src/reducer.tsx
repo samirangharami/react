2c667f240b13745f872395ef6c794bd438473b42
samirangharami <samirangharami05@gmail.com>
2026-04-20 23:24:49 +0530

    uses produce from immer

--- with-jsx/todo-app/src/reducer.tsx
+++ with-jsx/todo-app/src/reducer.tsx
@@ -1,29 +1,28 @@
-import { type State, type Action, ACTIONS } from "./types.tsx";
+import { produce } from "immer";
+import { type State, type Action, ACTIONS } from "./types";
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case ACTIONS.TOGGLE_TASK:
-      return {
-        ...state,
-        tasks: state.tasks.map((task) =>
-          task.id === action.id ? { ...task, isDone: !task.isDone } : task,
-        ),
-      };
+      return produce(state, (draft) => {
+        const task = draft.tasks.find((t) => t.id === action.id);
+        if (task) task.isDone = !task.isDone;
+      });
 
     case ACTIONS.DELETE_TASK:
-      return {
-        ...state,
-        tasks: state.tasks.filter((task) => task.id !== action.id),
-      };
+      return produce(state, (draft) => {
+        draft.tasks = draft.tasks.filter((t) => t.id !== action.id);
+      });
 
     case ACTIONS.ADD_TASK:
-      return {
-        nextId: state.nextId + 1,
-        tasks: [
-          ...state.tasks,
-          { id: state.nextId, title: action.title, isDone: false },
-        ],
-      };
+      return produce(state, (draft) => {
+        draft.tasks.push({
+          id: draft.nextId,
+          title: action.title,
+          isDone: false,
+        });
+        draft.nextId++;
+      });
 
     default:
       return state;
